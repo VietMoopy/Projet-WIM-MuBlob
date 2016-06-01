9dbea60f1fd28e7d76c1eaf8f79818578425a988
Vinlagrenouille <vincent.navales@etu.u-pec.fr>
2016-06-01 09:50:41 +0200

    correction 1 classes Admin, User, Review, Rating

--- createTables.sql
+++ createTables.sql
@@ -1,29 +1,33 @@
-create table RAdmin(
+create table MuAdmin(
 login varchar(20) primary key,
 password varchar(30), /* trigger here*/
 email varchar(30)
 );
 
-create table RUser(
+create table MuUser(
 email varchar(30) primary key,
 username varchar(20),
 password varchar(30), /* trigger here*/
 validity boolean
 );
 
-create table Review(
+create table MuReview(
 idReview int primary key AUTO_INCREMENT,
 message varchar(200),
 dateReview date,
 visibility boolean,
-email varchar(30) foreign key references RUser(email),
-idAlbum int foreign key references Album(idAlbum)
+email varchar(30),
+foreign key (email) references RUser(email),
+idAlbum int,
+foreign key (idAlbum) references Album(idAlbum)
 );
 
-create table Rating(
+create table MuRating(
 idRating int primary key AUTO_INCREMENT,
 value int, /*between 1 and 10*/
 dateRating date,
-email varchar(30) foreign key references RUser(email),
-idAlbum int foreign key references Album(idAlbum)
+email varchar(30),
+foreign key (email) references RUser(email),
+idAlbum int,
+foreign key (idAlbum) references Album(idAlbum)
 );
